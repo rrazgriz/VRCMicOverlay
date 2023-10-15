d7741bfde31a05359aa783d6a69a5f4dda026d3e
Raz <47901762+rrazgriz@users.noreply.github.com>
2023-10-14 21:13:10 -0400

    refactor: move Windows Stuff to static class

--- VRCMicOverlay/Program.cs
+++ VRCMicOverlay/Program.cs
@@ -113,7 +113,7 @@ namespace Raz.VRCMicOverlay
 
 #if !DEBUG // This gets annoying when developing
             // Minimize console window (6 corresponds to a native enum or something that means minimize)
-            ShowWindow(GetConsoleWindow(), 6);
+            WindowsUtilities.SetWindowState(WindowsUtilities.GetConsoleWindow(), WindowsUtilities.CMDSHOW.SW_MINIMIZE);
             Console.WriteLine("Minimizing Window");
 #endif
 
@@ -184,7 +184,7 @@ namespace Raz.VRCMicOverlay
             // Sound setup
             System.Media.SoundPlayer sfxMute = new(Config.FILENAME_SFX_MIC_MUTED);
             System.Media.SoundPlayer sfxUnmute = new(Config.FILENAME_SFX_MIC_UNMUTED);
-            SetVolume(Config.CUSTOM_MIC_SFX_VOLUME);
+            WindowsUtilities.SetVolume(Config.CUSTOM_MIC_SFX_VOLUME);
 
             // OSC Setup
             int oscPort = Config.LEGACY_OSC_LISTEN_PORT;
@@ -503,14 +503,37 @@ namespace Raz.VRCMicOverlay
 
 #endregion
 
-#region Windows
+    }
+
+    internal static class WindowsUtilities
+    {
+        public static bool SetWindowState(IntPtr hWnd, CMDSHOW command)
+        {
+            return ShowWindow(hWnd, (int)command);
+        }
 
         [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
         
         [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
+        public static extern IntPtr GetConsoleWindow();
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow?redirectedfrom=MSDN
+        public enum CMDSHOW
+        {
+            SW_HIDE = 0,
+            SW_SHOWNORMAL = 1,
+            SW_SHOWMINIMIZED = 2,
+            SW_SHOWMAXIMIZED = 3,
+            SW_SHOWNOACTIVATE = 4,
+            SW_MINIMIZE = 6,
+            SW_SHOWMINNOACTIVE = 7,
+            SW_SHOWNA = 8,
+            SW_RESTORE = 9,
+            SW_SHOWDEFAULT = 10,
+            SW_FORCEMINIMIZE = 11
+        }
 
         // thanks to https://stackoverflow.com/questions/34277066/how-do-i-fade-out-the-audio-of-a-wav-file-using-soundplayer-instead-of-stopping
         [DllImport("winmm.dll", EntryPoint = "waveOutGetVolume")]
@@ -519,18 +542,14 @@ namespace Raz.VRCMicOverlay
         [DllImport("winmm.dll", EntryPoint = "waveOutSetVolume")]
         private static extern int WaveOutSetVolume(IntPtr hwo, uint dwVolume);
 
-        private static int SetVolume(float volume)
+        public static int SetVolume(float volume)
         {
             float clampedVolume = MathF.Min(MathF.Max(volume, 0f), 1f);
-            ushort channelVolume = (ushort)Lerp(0, ushort.MaxValue, clampedVolume);
+            ushort channelVolume = (ushort)(ushort.MaxValue * clampedVolume);
             uint vol = (uint)channelVolume | ((uint)channelVolume << 16);
             return WaveOutSetVolume(IntPtr.Zero, vol);
         }
-
-#endregion
-
     }
-
 
     internal class MicListener
     {
