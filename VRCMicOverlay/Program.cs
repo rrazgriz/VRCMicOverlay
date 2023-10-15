0c622673df7d9c6895cff09271c5192412fb469d
Raz <47901762+rrazgriz@users.noreply.github.com>
2023-10-14 21:20:17 -0400

    refactor: move MicListener to separate file

--- VRCMicOverlay/Program.cs
+++ VRCMicOverlay/Program.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 using Valve.VR;
-using NAudio.Wave;
 using VRC.OSCQuery;
 
 namespace Raz.VRCMicOverlay
@@ -548,61 +547,6 @@ namespace Raz.VRCMicOverlay
             ushort channelVolume = (ushort)(ushort.MaxValue * clampedVolume);
             uint vol = (uint)channelVolume | ((uint)channelVolume << 16);
             return WaveOutSetVolume(IntPtr.Zero, vol);
-        }
-    }
-
-    internal class MicListener
-    {
-        public float MicLevel { get; private set; }
-
-        internal WaveInEvent waveIn;
-
-        public void SetupMicListener(Configuration configuration)
-        {
-            Console.WriteLine("\nAudio Device Selection:");
-            int deviceID = -1;
-            for (int i = 0; i < WaveInEvent.DeviceCount; i++)
-            {
-                var deviceCapabilities = WaveInEvent.GetCapabilities(i);
-                string deviceName = deviceCapabilities.ProductName;
-                if (deviceName.StartsWith(configuration.AUDIO_DEVICE_STARTS_WITH) && configuration.AUDIO_DEVICE_STARTS_WITH != "")
-                {
-                    Console.Write($"✓");
-                    deviceID = i;
-                }
-                else
-                {
-                    Console.Write($" ");
-                }
-                Console.WriteLine($" {i} : {deviceName}");
-            }
-            Console.WriteLine();
-            if(deviceID < 0) Console.WriteLine($"Audio Device \"{configuration.AUDIO_DEVICE_STARTS_WITH}\" not matched, using default recording device.\n");
-
-            waveIn = new WaveInEvent
-            {
-                DeviceNumber = deviceID,
-                // WaveFormat = new WaveFormat(rate: 44100, bits: 16, channels: 2),
-                BufferMilliseconds = 50
-            };
-
-            waveIn.DataAvailable += CalculatePeakMicLevel;
-            waveIn.StartRecording();
-        }
-
-        private void CalculatePeakMicLevel(object sender, WaveInEventArgs args)
-        {
-            const float maxValue = 32767;
-            const int bytesPerSample = 2;
-            
-            int peakValue = 0;
-            for (int index = 0; index < args.BytesRecorded; index += bytesPerSample)
-            {
-                int value = BitConverter.ToInt16(args.Buffer, index);
-                peakValue = Math.Max(peakValue, value);
-            }
-
-            MicLevel = peakValue / maxValue;
         }
     }
 }
