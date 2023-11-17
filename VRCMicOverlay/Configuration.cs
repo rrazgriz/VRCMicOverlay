7431eacdd7cc9ee10df537a79733ff399cc028a3
Raz <47901762+rrazgriz@users.noreply.github.com>
2023-11-17 00:49:44 -0500

    chore: reorder settings so more important ones are on top

--- VRCMicOverlay/Configuration.cs
+++ VRCMicOverlay/Configuration.cs
@@ -8,33 +8,34 @@ namespace Raz.VRCMicOverlay
         public float ICON_UNMUTED_MAX_ALPHA = 0.75f;    // Max icon transparency (0-1) while unmuted
         public float ICON_UNMUTED_MIN_ALPHA = 0.05f;    // Min icon transparency (0-1) while unmuted
 
-        public string ICON_TINT_MUTED = "#FF5F5F";      // Icon tint color while muted
-        public string ICON_TINT_UNMUTED = "#FFFFFF";    // Icon tint color while unmuted
-
-        public bool USE_CUSTOM_MIC_SFX = false;     // If true, will use custom sfx wav files when muting/unmuting
-        public float CUSTOM_MIC_SFX_VOLUME = 0.65f; // Volume (0-1) of custom sfx
-
-        // VRChat doesn't output the Voice parameter while muted, so we have to read from a device ourselves
-        public string AUDIO_DEVICE_STARTS_WITH = "";    // If this is blank, it'll use the default audio device
-        public float MUTED_MIC_THRESHOLD = 0.1f;        // Threshold (0-1) for the mic icon lighting up while muted
-
-        public float ICON_CHANGE_SCALE_FACTOR = 1.25f; // Scale icon by this factor when changing between mute/unmute. Set to 1.0 to disable
-
-        public bool ICON_ALWAYS_ON_TOP = true;          // Whether the icon should show over all other overlays or not
-        public bool ICON_RANDOMIZED_OFFSET = true;      // Randomize icon position about the offset point on startup. May help reduce burn-in/burn-out on (u)OLED HMDs.
-        public float ICON_SIZE = 0.05f;                 // Size, square, of icon overlay (in meters)
-        public float ICON_OFFSET_X = -0.37f;            // Distance left/right of head center axis (negative is left)
-        public float ICON_OFFSET_Y = -0.26f;            // Distance above/below head axis (negative is in front)
-        public float ICON_OFFSET_Z = -0.92f;            // Distance in front of the head (negative is in front)
-
-        public bool RESTART_FADE_TIMER_ON_STATE_CHANGE = true; // Whether to restart the fade timer when changing from mute/unmute. Play with this to see if you like it
         public float MIC_MUTED_FADE_START    = 1.0f;    // Time before icon starts fading while muted (times are in seconds)
         public float MIC_MUTED_FADE_PERIOD   = 2.0f;    // Time to fade from max to min alpha while muted
         public float MIC_UNMUTED_FADE_START  = 0.3f;    // Time before icon starts fading while unmuted
         public float MIC_UNMUTED_FADE_PERIOD = 1.0f;    // Time to fade from max to min alpha while unmuted
+        public bool RESTART_FADE_TIMER_ON_STATE_CHANGE = true; // Whether to restart the fade timer when changing from mute/unmute. Play with this to see if you like it
 
-        public float ICON_UNFADE_TIME = 0.05f; // Time to go from faded to unfaded
-        public float ICON_ALPHA_EXPONENT = 2.0f; // Scaling exponent for alpha. Recommend keeping at 2.0, reducing if long fade times are used.
+        public string ICON_TINT_MUTED = "#FF5F5F";      // Icon tint color while muted
+        public string ICON_TINT_UNMUTED = "#FFFFFF";    // Icon tint color while unmuted
+
+        public bool ICON_ALWAYS_ON_TOP = true;          // Whether the icon should show over all other overlays or not
+        public bool ICON_RANDOMIZED_OFFSET = true;      // Randomize icon position about the offset point on startup. May help reduce burn-in/burn-out on (u)OLED HMDs.
+
+        // VRChat doesn't output the Voice parameter while muted, so we have to read from a device ourselves
+        public string AUDIO_DEVICE_STARTS_WITH = "";    // Enter the first unique characters of your audio device's name (off the list printed out when the app starts. If this is blank, it'll use the default audio device
+        public float MUTED_MIC_THRESHOLD = 0.1f;        // Threshold (0-1) for the mic icon lighting up while muted. Recommend leaving at 0.1 for most mics, 0.2 if it's too sensitive
+
+        public float ICON_SIZE = 0.05f;                 // Size, square, of icon overlay (in meters)
+        public float ICON_CHANGE_SCALE_FACTOR = 1.25f;  // Scale icon by this factor when changing between mute/unmute (how big it "Bounces"). Set to 1.0 to disable
+
+        public float ICON_OFFSET_X = -0.37f;            // Distance left/right of head center axis (negative is left)
+        public float ICON_OFFSET_Y = -0.26f;            // Distance above/below head axis (negative is in front)
+        public float ICON_OFFSET_Z = -0.92f;            // Distance in front of the head (negative is in front)
+
+        public float ICON_UNFADE_TIME = 0.05f;          // Time to go from faded to unfaded
+        public float ICON_ALPHA_EXPONENT = 2.0f;        // Scaling exponent for alpha. Recommend keeping at 2.0, reducing if long fade times are used.
+
+        public bool USE_CUSTOM_MIC_SFX = false;     // If true, will use custom sfx wav files when muting/unmuting
+        public float CUSTOM_MIC_SFX_VOLUME = 0.65f; // Volume (0-1) of custom sfx
 
         public string FILENAME_SFX_MIC_UNMUTED = "Assets/sfx-unmute.wav";          // Custom sound for unmute (must be wav)
         public string FILENAME_SFX_MIC_MUTED = "Assets/sfx-mute.wav";              // Custom sound for mute (must be wav)
